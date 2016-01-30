c1499ce6260f162b54ae8f58e20688a8cd56ec57
Will Metz <willmetz@gmail.com>
2016-01-30 14:40:12 -0500

    Corrected divider lines being drawn over top of the headers

--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/Views/SimpleDividerLineDecorator.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/Views/SimpleDividerLineDecorator.java
@@ -29,7 +29,8 @@ public class SimpleDividerLineDecorator extends RecyclerView.ItemDecoration
         }
 
         @Override
-        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state)
+        {
             int left = parent.getPaddingLeft();
             int right = parent.getWidth() - parent.getPaddingRight();
 
